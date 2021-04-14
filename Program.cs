3afbfb2ce1db2c2ba28d7e4edebe209d93c3996e
Muhammad shoaib <muhammadshoaib0k0@gmail.com>
2021-04-14 14:27:20 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Countword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            int count = 1 , len=0;
+            Console.WriteLine("Enter a string :  ");
+            str = Console.ReadLine();
+
+            while (len<=str.Length-1)
+            {    // use single ' ' quation not use double quation " "
+                if(str[len]==' ' || str[len]=='\n' || str[len]=='\t')
+                {
+                    count++;
+                }
+                len++;
+            }
+            Console.WriteLine("total no of words are : " + count);
+            Console.ReadLine();
+        }
+    }
+}
